9828cfd9ea44fe987738b0f0c34122d8c4b1781d
pheldu95 <phelps.duncan@gmail.com>
2025-11-25 10:03:09 -0600

    added other type definitions to index.d.ts

--- client/src/lib/types/index.d.ts
+++ client/src/lib/types/index.d.ts
@@ -1,7 +1,32 @@
 // typescript definition file
 type FishSpecies = {
-    id: number;
-    name: string;
+  id: number
+  name: string
+  caughtFish: string[]
 };
 
-export type { FishSpecies };
+type CaughtFish = {
+  id: number
+  latitude: string
+  longitude: string
+  caughtDate: string
+  caughtBy: string
+  fishSpecies: string
+  fishingLure: string
+};
+
+type FishingLure = {
+  id: number
+  name: string
+  color: string
+  addedBy: string
+  createdAt: string
+  caughtFish: string[]
+};
+
+type User = {
+    id: number
+    email: string
+}
+
+export type { FishSpecies, CaughtFish, FishingLure, User };
