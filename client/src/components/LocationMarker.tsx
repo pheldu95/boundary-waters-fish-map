0b561a45083e589f66a371d35bc596f97e7bef65
pheldu95 <phelps.duncan@gmail.com>
2025-12-01 10:14:16 -0600

    marker popup opens immediately

--- client/src/components/LocationMarker.tsx
+++ client/src/components/LocationMarker.tsx
@@ -1,8 +1,18 @@
-import { useState } from 'react'
+import type { Marker as LeafletMarker} from 'leaflet';
+import { useEffect, useRef, useState } from 'react'
 import { Marker, Popup, useMapEvents } from 'react-leaflet'
 
 export default function LocationMarker() {
     const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
+
+    const markerRef = useRef<LeafletMarker | null>(null);
+
+    useEffect(() => {
+        if (position.latitude !== 0 && position.longitude !== 0 && markerRef.current) {
+            markerRef.current.openPopup();
+        }
+    }, [position]);
+
     const map = useMapEvents({
         click(e) {
             const { lat, lng } = e.latlng;
@@ -10,13 +20,24 @@ export default function LocationMarker() {
                 latitude: lat,
                 longitude: lng,
             });
-            map.flyTo(e.latlng, map.getZoom())
+            map.flyTo(e.latlng, map.getZoom());
         },
     });
 
-    return position === null ? null : (
-        <Marker position={[position.latitude, position.longitude]}>
-            <Popup>Caught a fish here</Popup>
+    if (position.latitude === 0 && position.longitude === 0) return null;
+
+    return (
+        <Marker
+            position={[position.latitude, position.longitude]}
+            ref={markerRef}
+        >
+            <Popup>
+                <form>
+                    <label>Test </label>
+                    <input type="text" />
+                    <br />
+                </form>
+            </Popup>
         </Marker>
     )
 }
