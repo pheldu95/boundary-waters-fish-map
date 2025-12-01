fe8b038f62b963076906fd6e69f760037486090f
pheldu95 <phelps.duncan@gmail.com>
2025-11-30 22:37:44 -0600

    added mkcert to project so can use https for development instead of http

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    mkcert()
   ],
   server: {
     host: true,
