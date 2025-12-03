b856e61643a1593bb50e06c8f609e6f7717378ec
pheldu95 <phelps.duncan@gmail.com>
2025-12-03 13:35:22 -0600

    more styling on the caught fish form

--- client/src/features/caughtFish/CaughtFishForm.tsx
+++ client/src/features/caughtFish/CaughtFishForm.tsx
@@ -48,33 +48,65 @@ export default function CaughtFishForm({ latitude, longitude, markerRef }: Props
   }
 
   return (
-    <form onSubmit={handleSubmit} className='flex flex-col gap-2'>
-      <label htmlFor='caughtDate'>Date</label>
-      <input id='caughtDate' name='caughtDate' type="date" />
+    <form onSubmit={handleSubmit} className='flex flex-col gap-2 w-full items-center'>
+      <div className="w-[90%]">
+        <label htmlFor='caughtDate' className="mb-1 block text-s font-small">Date</label>
+        <input
+          id='caughtDate'
+          name='caughtDate'
+          type="date"
+          className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-small text-grey outline-none focus:border-foresty focus:shadow-md"
+        />
+      </div>
 
-      <label htmlFor='fishSpecies'>Species </label>
-      <select id='fishSpecies' name="fishSpecies" value={selectedFishSpecies} onChange={handleSpeciesChange}>
-        <option value="" disabled hidden>
-          Choose...
-        </option>
-        {fishSpecies?.map((species) => (
-          <option key={species.id} value={`/api/fish_species/${species.id}`}>{species.name}</option>
-        ))}
-      </select>
+      <div className="w-[90%]">
+        <label htmlFor='fishSpecies' className="mb-1 block text-s font-small">Species </label>
+        <select
+          id='fishSpecies'
+          name="fishSpecies"
+          value={selectedFishSpecies}
+          onChange={handleSpeciesChange}
+          className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-small text-grey outline-none focus:border-foresty focus:shadow-md"
+        >
+          <option value="" disabled hidden>
+            Choose...
+          </option>
+          {fishSpecies?.map((species) => (
+            <option key={species.id} value={`/api/fish_species/${species.id}`}>{species.name}</option>
+          ))}
+        </select>
+      </div>
 
-      <label htmlFor='fishingLure'>Lure</label>
-      <select id='fishingLure' defaultValue="" name="fishingLure" value={selectedFishingLure} onChange={handleLureChange}>
-        <option value="" disabled hidden>
-          Choose...
-        </option>
-        {fishingLures?.map((lure) => (
-          <option key={lure.id} value={`/api/fishing_lures/${lure.id}`}>{lure.name}</option>
-        ))}
-      </select>
+      <div className="w-[90%]">
+        <label htmlFor='fishingLure' className="mb-1 block text-s font-small">Lure</label>
+        <select
+          id='fishingLure'
+          defaultValue=""
+          name="fishingLure"
+          value={selectedFishingLure}
+          onChange={handleLureChange}
+          className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-small text-grey outline-none focus:border-foresty focus:shadow-md"
+        >
+          <option value="" disabled hidden>
+            Choose...
+          </option>
+          {fishingLures?.map((lure) => (
+            <option key={lure.id} value={`/api/fishing_lures/${lure.id}`}>{lure.name}</option>
+          ))}
+        </select>
+      </div>
 
-      <button onClick={toggleAddNote} type='button'>{addNote ? 'Cancel' : 'Add Note'}</button>
-      <label htmlFor='note'>Note</label>
-      <textarea id='note' name='note' />
+      <button className='cursor-pointer w-24 hover:text-forestyhover transition-colors' onClick={toggleAddNote} type='button'>{addNote ? 'Cancel' : 'Add Note'}</button>
+
+      <div className={`w-full overflow-hidden transition-all duration-300 ease-in-out ${addNote ? 'max-h-40 opacity-100' : 'max-h-0 opacity-0'
+        }`}>
+        <label htmlFor='note' className="mb-1 block text-s font-small">Note</label>
+        <textarea
+          id='note'
+          name='note'
+          className='w-full rounded-md border border-[#e0e0e0] bg-white py-1 px-6 text-base font-small text-grey outline-none focus:border-foresty focus:shadow-md'
+        />
+      </div>
 
       <input type="hidden" name="latitude" value={latitude} />
       <input type="hidden" name="longitude" value={longitude} />
